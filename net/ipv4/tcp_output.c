76cc35b1e472f1254b9c8966d6cd1f86a0dd152b
MatteoGuarna <s303434@studenti.polito.it>
2023-11-29 14:06:05 +0100

    added SYNACK marking

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -3845,6 +3845,8 @@ struct sk_buff *tcp_make_synack(const struct sock *sk, struct dst_entry *dst,
 	th->window = htons(min(req->rsk_rcv_wnd, 65535U));
 	tcp_options_write((__be32 *)(th + 1), NULL, &opts);
 	th->doff = (tcp_header_size >> 2);
+	/*SPIN BIT impl: mark the header of the SYNACK packet*/
+	th->time |= 0b1;
 	__TCP_INC_STATS(sock_net(sk), TCP_MIB_OUTSEGS);
 
 #ifdef CONFIG_TCP_MD5SIG
